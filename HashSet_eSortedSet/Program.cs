1fe2291c5474ebf4e40299073c8c75e01ed09eb0
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-08 15:57:57 -0300

    verificando contém

--- HashSet_eSortedSet/Program.cs
+++ HashSet_eSortedSet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HashSet_eSortedSet
 {
@@ -6,7 +7,14 @@ namespace HashSet_eSortedSet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> conjunto = new HashSet<string>();
+
+            conjunto.Add("TV");
+            conjunto.Add("Notebook");
+            conjunto.Add("Tablet");
+
+            Console.WriteLine(conjunto.Contains("Computer")); //vai verificar se no conjunto contem Computer
+
         }
     }
 }
