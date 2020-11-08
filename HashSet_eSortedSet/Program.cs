cd066694dd0bb614ddad592bcb03f8ee2cfdf505
Angelo Carvalho <angelosuporteti@gmail.com>
2020-11-08 20:53:45 -0300

    União

--- HashSet_eSortedSet/Program.cs
+++ HashSet_eSortedSet/Program.cs
@@ -8,10 +8,18 @@ namespace HashSet_eSortedSet
         static void Main(string[] args)
         {
             SortedSet<int> conjuntoA = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 9 };
-            SortedSet<int> conjuntoB = new SortedSet<int>() { 5, 7, 8, 9, 10 };
+            SortedSet<int> conjuntoB = new SortedSet<int>() { 1, 3, 5, 7, 8, 9, 10 };
 
             ImprimirConjunto(conjuntoA);
             ImprimirConjunto(conjuntoB);
+
+            Console.WriteLine();
+            Console.Write("União: ");
+            SortedSet<int> conjuntoC = new SortedSet<int>(conjuntoA);
+            conjuntoC.UnionWith(conjuntoB);
+            ImprimirConjunto(conjuntoC);
+
+
 
             Console.ReadKey();
         }
